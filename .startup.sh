d31b840d8083d794f020f1efa4371e33851c9e48
Ben Hipple <bhipple@bloomberg.net>
2014-05-21 19:12:17 -0400

    merged and updated gitignore, other files

---
+++ .startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Open programs on monitors
+DISPLAY=:0.1
+konsole nohup
+
+DISPLAY=:0.2
+konsole nohup
+
+DISPLAY=:0.4
+konsole nohup
+
+DISPLAY=:0.5
+konsole nohup
+
+DISPLAY=:0.3
+konsole nohup
+skype &
+
+
+
+DISPLAY=:0.0
