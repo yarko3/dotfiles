64d119ad6de03cae8ba6386397a59ec4b19e6e68
Ben Hipple <bhipple@bloomberg.net>
2014-06-08 15:10:30 -0400

    added opera and chromium-browser

--- .install_programs.sh
+++ .install_programs.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
+# Install script for setting up all of my programs on Linux
 
-
+#==============================================================================
 # Adding Repos
+#
 sudo apt-add-repository ppa:jtaylor/keepass
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 
-# Upddate
+# Opera
+sudo sh -c 'echo "deb http://deb.opera.com/opera/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
+sudo sh -c 'wget -O - http://deb.opera.com/archive.key | apt-key add -'
+
+
+#==============================================================================
+# Update
+#
 sudo apt-get update
 
+
+#==============================================================================
 # Specific Installs
+#
 sudo apt-get install keepass2
 sudo apt-get install skype
 sudo apt-get install flashplugin-installer
 sudo apt-get install wireshark
 sudo apt-get install filezilla
+sudo apt-get install chromium-browser
+sudo apt-get install opera
 
+
+#==============================================================================
 # Generic Upgrade
+#
 sudo apt-get upgrade
--- .startup.sh
+++ .startup.sh
@@ -6,25 +6,35 @@ then
 fi
 export RAN_STARTUP="TRUE"
 
+#==============================================================================
 # Open programs on monitors
+#
+# Top Center
 DISPLAY=:0.1
-nohup konsole &
-
-DISPLAY=:0.2
-nohup konsole &
-
-DISPLAY=:0.4
 nohup konsole &
 nohup firefox &
 
+# Top Right
+DISPLAY=:0.2
+nohup konsole &
+nohup filezilla &
+
+# Bottom Right
+DISPLAY=:0.4
+nohup konsole &
+nohup chromium-browser &
+
+# Top Right
 DISPLAY=:0.5
 nohup konsole &
 nohup keepass2 &
 
+# Bottom Left
 DISPLAY=:0.3
 nohup konsole &
 nohup skype &
+nohup opera &
 
-
+# Bottom Center
 DISPLAY=:0.0
 nohup google-chrome &
