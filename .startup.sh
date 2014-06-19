14d5fd0f2b8d59d9971a30f7af49d59ee5ced968
Ben Hipple <benjamin.hipple@gmail.com>
2014-06-19 00:52:49 -0400

    switched to FF for default browser

--- .startup.sh
+++ .startup.sh
@@ -5,18 +5,18 @@
 # Top Center
 DISPLAY=:0.1
 nohup konsole &
-nohup ~/.chrome-x-launch.sh &
+nohup ~/.firefox-x-launch.sh &
 
 # Top Left
 DISPLAY=:0.2
 nohup konsole &
 nohup steam &
-nohup ~/.chrome-x-launch.sh &
+nohup ~/.firefox-x-launch.sh &
 
 # Bottom Right
 DISPLAY=:0.4
 nohup konsole &
-nohup ~/.chrome-x-launch.sh &
+nohup ~/.firefox-x-launch.sh &
 
 # Top Right
 DISPLAY=:0.5
@@ -27,8 +27,8 @@ nohup keepass2 &
 DISPLAY=:0.3
 nohup konsole &
 nohup skype &
-nohup google-chrome &
+nohup ~/.firefox-x-launch.sh &
 
 # Bottom Center
 DISPLAY=:0.0
-nohup ~/.chrome-x-launch.sh &
+nohup ~/.firefox-x-launch.sh &
