cf55317441b3e113f186e6c63265c3f216f51e7d
Ben Hipple <benjamin.hipple@gmail.com>
2014-06-19 00:51:24 -0400

    fixed firefox x launch

--- .firefox-x-launch.sh
+++ .firefox-x-launch.sh
@@ -1,2 +1,22 @@
-#!/bin/sh
-exec /usr/bin/firefox--enable-greasemonkey --enable-user-scripts --enable-extensions --user-data-dir=~/.mozilla/$DISPLAY "$@"
+#!/bin/bash
+# To create these profiles, run:
+#    $ firefox -ProfileManager
+
+if [ "$DISPLAY" == ':0.0' ]; then
+    nohup firefox -no-remote -P BottomCenter &
+
+elif [ "$DISPLAY" == ':0.1' ]; then
+    nohup firefox -no-remote -P TopCenter &
+
+elif [ "$DISPLAY" == ':0.2' ]; then
+    nohup firefox -no-remote -P TopLeft &
+
+elif [ "$DISPLAY" == ':0.3' ]; then
+    nohup firefox -no-remote -P BottomLeft &
+
+elif [ "$DISPLAY" == ':0.4' ]; then
+    nohup firefox -no-remote -P BottomRight &
+
+elif [ "$DISPLAY" == ':0.5' ]; then
+    nohup firefox -no-remote -P TopRight &
+fi
