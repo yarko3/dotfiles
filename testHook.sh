bec6b3f921a1fa9a8e99dd64fd819951f75c9165
Ben Hipple <benjamin.hipple@gmail.com>
2014-07-30 01:10:45 -0400

    Updating testHook for clang WIP

--- testHook.sh
+++ testHook.sh
@@ -29,7 +29,16 @@ if [ -n "$(git diff-index --cached --check HEAD --)" ]; then
         echo $line
     done
 
+    # TODO - Fix the whitespace automatically with sed and re-add it to
+    # staging before the commit :D
     exit 1
+fi
+
+
+# TODO - Automatic integration with --clang-format?
+if [ -n "" ]; then
+    echo "HOOK Running clang-format"
+    eval "git diff -U0 --cached HEAD | clang-format-diff.py -i -pl"
 fi
 
 exit 0
