e613c3f366dfb86e02bacb171a071db4b239535b
Ben Hipple <bhipple@bloomberg.net>
2014-08-17 22:14:04 -0400

    fixing firefox launch script

--- .x_server/firefox_launch.sh
+++ .x_server/firefox_launch.sh
@@ -2,21 +2,31 @@
 # To create these profiles, run:
 #    $ firefox -ProfileManager
 
+
+# Monitors
+BottomCenter=0
+TopCenter=1
+TopLeft=2
+BottomLeft=3
+BottomRight=4
+TopRight=5
+
+
 if [ "$DISPLAY" == ':0.0' ]; then
-    nohup firefox -no-remote -P BottomCenter &
+    nohup firefox -no-remote -P $BottomCenter &
 
 elif [ "$DISPLAY" == ':0.1' ]; then
-    nohup firefox -no-remote -P TopCenter &
+    nohup firefox -no-remote -P $TopCenter &
 
 elif [ "$DISPLAY" == ':0.2' ]; then
-    nohup firefox -no-remote -P TopLeft &
+    nohup firefox -no-remote -P $TopLeft &
 
 elif [ "$DISPLAY" == ':0.3' ]; then
-    nohup firefox -no-remote -P BottomLeft &
+    nohup firefox -no-remote -P $BottomLeft &
 
 elif [ "$DISPLAY" == ':0.4' ]; then
-    nohup firefox -no-remote -P BottomRight &
+    nohup firefox -no-remote -P $BottomRight &
 
 elif [ "$DISPLAY" == ':0.5' ]; then
-    nohup firefox -no-remote -P TopRight &
+    nohup firefox -no-remote -P $TopRight &
 fi
