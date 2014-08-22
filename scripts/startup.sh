086f4162a1d138c4ad45dc858b58e636edfa95de
Ben Hipple <bhipple@bloomberg.net>
2014-08-22 17:55:42 -0400

    Reorganized directory into scripts folder

---
+++ scripts/startup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+## ============================================================================
+##                         Open Programs On Monitors
+## ============================================================================
+# Top Center
+DISPLAY=:0.1
+nohup konsole &
+nohup ~/scripts/x_server/firefox_launch.sh &
+
+# Top Left
+DISPLAY=:0.2
+nohup konsole &
+nohup filezilla &
+nohup ~/scripts/x_server/firefox_launch.sh &
+
+# Bottom Right
+DISPLAY=:0.4
+nohup konsole &
+nohup ~/scripts/x_server/firefox_launch.sh &
+
+# Top Right
+DISPLAY=:0.5
+nohup konsole &
+nohup keepass2 &
+
+# Bottom Left
+DISPLAY=:0.3
+nohup konsole &
+nohup skype &
+nohup ~/scripts/x_server/firefox_launch.sh &
+
+# Bottom Center
+DISPLAY=:0.0
+nohup ~/scripts/x_server/firefox_launch.sh &
