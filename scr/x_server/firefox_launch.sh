68eef94bff284d60aace14733f4983056c46e9da
bhipple <bhipple@bloomberg.net>
2014-10-05 22:27:54 -0400

    got rid of nohup on firefox

--- scr/x_server/firefox_launch.sh
+++ scr/x_server/firefox_launch.sh
@@ -13,20 +13,20 @@ TopRight=5
 
 
 if [ "$DISPLAY" == ':0.0' ]; then
-    nohup firefox -no-remote -P $BottomCenter &
+    firefox -no-remote -P $BottomCenter &
 
 elif [ "$DISPLAY" == ':0.1' ]; then
-    nohup firefox -no-remote -P $TopCenter &
+    firefox -no-remote -P $TopCenter &
 
 elif [ "$DISPLAY" == ':0.2' ]; then
-    nohup firefox -no-remote -P $TopLeft &
+    firefox -no-remote -P $TopLeft &
 
 elif [ "$DISPLAY" == ':0.3' ]; then
-    nohup firefox -no-remote -P $BottomLeft &
+    firefox -no-remote -P $BottomLeft &
 
 elif [ "$DISPLAY" == ':0.4' ]; then
-    nohup firefox -no-remote -P $BottomRight &
+    firefox -no-remote -P $BottomRight &
 
 elif [ "$DISPLAY" == ':0.5' ]; then
-    nohup firefox -no-remote -P $TopRight &
+    firefox -no-remote -P $TopRight &
 fi
