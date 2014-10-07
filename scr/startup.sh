82cbffdc37c64a228acf62b9c4d3e1ad97da430c
bhipple <bhipple@bloomberg.net>
2014-10-07 00:13:10 -0400

    updating scripts to play nicer with awesome

--- scr/startup.sh
+++ scr/startup.sh
@@ -4,32 +4,28 @@
 ## ============================================================================
 # Top Center
 DISPLAY=:0.1
-nohup konsole &
-nohup ~/scr/x_server/firefox_launch.sh &
+konsole &
+~/scr/x_server/firefox_launch.sh &
 
 # Top Left
 DISPLAY=:0.2
-nohup konsole &
-nohup filezilla &
-nohup ~/scr/x_server/firefox_launch.sh &
+konsole &
+~/scr/x_server/firefox_launch.sh &
 
 # Bottom Right
 DISPLAY=:0.4
-nohup konsole &
-nohup ~/scr/x_server/firefox_launch.sh &
+konsole &
+~/scr/x_server/firefox_launch.sh &
 
 # Top Right
 DISPLAY=:0.5
-nohup konsole &
-nohup keepass2 &
-nohup ~/scr/x_server/firefox_launch.sh &
+konsole &
+~/scr/x_server/firefox_launch.sh &
 
 # Bottom Left
 DISPLAY=:0.3
-nohup konsole &
-nohup skype &
-nohup ~/scr/x_server/firefox_launch.sh &
+konsole &
+skype &
 
 # Bottom Center
 DISPLAY=:0.0
-nohup ~/scr/x_server/firefox_launch.sh &
--- scr/x_server/chrome_launch.sh
+++ scr/x_server/chrome_launch.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-exec /opt/google/chrome/google-chrome --enable-greasemonkey --enable-user-scripts --enable-extensions --user-data-dir=~/.config/google-chrome/$DISPLAY "$@"
+#exec /opt/google/chrome/google-chrome --enable-greasemonkey --enable-user-scripts --enable-extensions --user-data-dir=~/.config/google-chrome/$DISPLAY "$@"
+
+# False advertising on the script name :)
+chromium-browser
