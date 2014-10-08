aab0e6f0ce456f9f544c59491754746faac57751
bhipple <bhipple@bloomberg.net>
2014-10-07 22:59:24 -0400

    updating DISPLAY handler scripts

--- scr/display/export_disp.sh
+++ scr/display/export_disp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-rm -f ~/scr/display.txt
+
+cd ~/scr/display/
+[ -f display.txt ] && echo "Previous display value: " && eval cat display.txt
+rm -f display.txt
+
 echo "$DISPLAY" >> display.txt
+echo "New display value: "
+eval cat display.txt
--- scr/display/import_disp.sh
+++ scr/display/import_disp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-disp=$(cat display.txt)
+disp=$(cat ~/scr/display/display.txt)
 export DISPLAY=$disp
+
+eval echo "Set display to $(cat ~/scr/display/display.txt)"
