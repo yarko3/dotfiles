d4e9feb3e9feadc4876cbf7e12c60f77630bea6b
bhipple <bhipple@bloomberg.net>
2014-10-24 13:06:20 -0400

    cd scraper for secondscrape and thirdscrape

--- scr/cd_scraper.sh
+++ scr/cd_scraper.sh
@@ -4,11 +4,19 @@ if [ -z $1 ]; then
     exit 1;
 fi
 
+prefix=~/mbig/scrape.git/msgscrape
 dir=$1
+
+if [[ $2 == '2' ]]; then
+    prefix=~/mbig/secondscrape.git/msgscrape
+elif [[ $2 == '3' ]]; then
+    prefix=~/mbig/thirdscrape.git/msgscrape
+fi
+
 if [ $dir == 'scrpxbsvc' ]; then
     dir=pcs_xb_mapping/$dir
 elif [ $dir == 'scrpxbdb' ]; then
     dir=pcs_xb_mapping/db
 fi
 
-eval cd ~/mbig/scrape.git/msgscrape/$dir
+eval cd $prefix/$dir
