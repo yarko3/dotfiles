6244d85f9748d0592d41cf835535bf553ea5aea5
bhipple <bhipple@bloomberg.net>
2014-12-13 21:10:44 -0500

    pacmd shellscript improvements

--- scr/pacmd_audio.sh
+++ scr/pacmd_audio.sh
@@ -2,43 +2,38 @@
 # Change audio between HDMI | Headphones, and move all input sinks
 
 # Look up these values with `$ pacmd list-sinks | less`
-outputSink=1
-headphones_sink=1
-
-# Bottom center monitor
-hdmi_sink=2
-
-# Bottom right monitor
-other_hdmi_sink=0
-
+headphones=1
+hdmi_bottom_center=2
+hdmi_bottom_right=0
 
 # Read input arg and figure out desired sink
 set_output_sink() {
     # Default to headphones
     if [ -z $1 ]; then
         echo "Switching to Headphones"
-        outputSink=$headphones_sink
+        return $headphones
     elif [ "$1" == "hdmi" ]; then
         echo "Switching to HDMI"
-        outputSink=$hdmi_sink
+        return $hdmi_bottom_center
     else
         echo "Switching to sink $1"
-        outputSink=$1
+        return $1
     fi
 }
 
 move_sinks() {
 
-    pacmd set-default-sink $outputSink
+    pacmd set-default-sink $1
 
     # Move the input sinks to the output sink
     pacmd list-sink-inputs | grep index | awk ' {
             print "pacmd move-sink-input " $2 " " outSink "\0"
-        }' outSink="$outputSink" | xargs -0 bash -c
+        }' outSink="$1" | xargs -0 bash -c
 
     echo ""
 }
 
 
+# $? is the return value of the first function call
 set_output_sink $1
-move_sinks
+move_sinks $?
