6af504728950d0f1d19152e453d95595ce1f943b
Benjamin Hipple <bhipple@bloomberg.net>
2015-02-18 18:48:42 -0500

    Pruned git aliases; updated YCM installer

--- scr/ycm_bb_install.sh
+++ scr/ycm_bb_install.sh
@@ -6,10 +6,10 @@ mkdir ycm_build
 cd ycm_build
 
 # Without clang / c support
-#cmake -G "Unix Makefiles" -DPYTHON_LIBRARY=/opt/swt/lib64/libpython2.7.so -DPYTHON_INCLUDE_DIR=/opt/swt/include/python2.7 . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake -G "Unix Makefiles" -DPYTHON_LIBRARY=/opt/swt/lib64/libpython2.7.so -DPYTHON_INCLUDE_DIR=/opt/swt/include/python2.7 . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 
 # With clang
-cmake -G "Unix Makefiles" -DPYTHON_LIBRARY=/opt/swt/lib64/libpython2.7.so -DPYTHON_INCLUDE_DIR=/opt/swt/include/python2.7 -DEXTERNAL_LIBCLANG_PATH=/bb/sys/package/opt/bb/lib64/libclang.so . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+#cmake -G "Unix Makefiles" -DPYTHON_LIBRARY=/opt/swt/lib64/libpython2.7.so -DPYTHON_INCLUDE_DIR=/opt/swt/include/python2.7 -DEXTERNAL_LIBCLANG_PATH=/bb/sys/package/opt/bb/lib64/libclang.so . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 
 make ycm_support_libs
 cd ..
