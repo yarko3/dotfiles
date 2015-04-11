e6a0657843ac0555243181f48f27531ee7932eef
Benjamin Hipple <bhipple@bloomberg.net>
2015-04-10 22:34:08 -0400

    llvm and clang build script

--- scr/clang_install.sh
+++ scr/clang_install.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+BASE_DIR="/bb/bigstorq4/scrpbuild/ben"
+mkdir -p "$BASE_DIR"
+
 clone_repos() {
-    cd ~/
+    cd "$BASE_DIR"
     git clone http://llvm.org/git/llvm.git
 
-    cd ~/llvm/tools
+    cd "$BASE_DIR"/llvm/tools
     git clone http://llvm.org/git/clang.git
 
-    cd ~/llvm/projects
+    cd "$BASE_DIR"/llvm/projects
     git clone http://llvm.org/git/compiler-rt.git
     git clone http://llvm.org/git/test-suite.git
 
-    cd ~/llvm/tools/clang/tools
+    cd "$BASE_DIR"/llvm/tools/clang/tools
     git clone http://llvm.org/git/clang-tools-extra.git
 }
 
 build() {
-    cd ~/
+    cd "$BASE_DIR"
     mkdir -p build && cd build
     ../llvm/configure
     make
     #sudo make install # hmm?
 }
+
+clone_repos
+build
