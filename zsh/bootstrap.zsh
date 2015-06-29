2050ab8d93cc67543eb2a0bb1790a4d44cf37c0b
Benjamin Hipple <bhipple@bloomberg.net>
2015-06-29 17:20:33 -0400

    Fix path

--- zsh/bootstrap.zsh
+++ zsh/bootstrap.zsh
@@ -2,4 +2,4 @@ cd
 ln -fs /home/bhipple/dotfiles_local /home/bhipple/.dotfiles_local
 cd -
 
-PATH=~/.dotfiles/bin:~/.dotfiles_local/bin:${PATH}
+PATH=~/.dotfiles/bin:~/.dotfiles_local/bin_local:${PATH}
