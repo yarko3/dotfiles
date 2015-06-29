cf927ccf4007f981329181441629a3cf3463188c
Benjamin Hipple <bhipple@bloomberg.net>
2015-06-29 17:18:46 -0400

    force symlink

--- zsh/bootstrap.zsh
+++ zsh/bootstrap.zsh
@@ -1,5 +1,5 @@
 cd
-ln -s /home/bhipple/dotfiles_local /home/bhipple/.dotfiles_local
+ln -fs /home/bhipple/dotfiles_local /home/bhipple/.dotfiles_local
 cd -
 
 PATH=~/.dotfiles/bin:~/.dotfiles_local/bin:${PATH}
