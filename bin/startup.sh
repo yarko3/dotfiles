53943aba4a95b08b8b7551cd4b50e6a6e0d55c5e
Benjamin Hipple <brh2113@columbia.edu>
2015-07-11 20:59:26 -0400

    Fix path on startup script

--- bin/startup.sh
+++ bin/startup.sh
@@ -5,22 +5,22 @@
 # Top Center
 DISPLAY=:0.1
 konsole &
-~/scr/x_server/firefox_launch.sh &
+~/bin/firefox_launch.sh &
 
 # Top Left
 DISPLAY=:0.2
 konsole &
-~/scr/x_server/firefox_launch.sh &
+~/bin/firefox_launch.sh &
 
 # Bottom Right
 DISPLAY=:0.4
 konsole &
-~/scr/x_server/firefox_launch.sh &
+~/bin/firefox_launch.sh &
 
 # Top Right
 DISPLAY=:0.5
 konsole &
-~/scr/x_server/firefox_launch.sh &
+~/bin/firefox_launch.sh &
 
 # Bottom Left
 DISPLAY=:0.3
@@ -29,4 +29,4 @@ skype &
 
 # Bottom Center
 DISPLAY=:0.0
-~/scr/x_server/firefox_launch.sh &
+~/bin/firefox_launch.sh &
