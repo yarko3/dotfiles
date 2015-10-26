cc180fd49a4bc0070089606388f4f342460867d8
Benjamin Hipple <bhipple@bloomberg.net>
2015-10-26 16:25:20 -0400

    Get the Chef developer kit through chef.io

--- dependency_install.sh
+++ dependency_install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-sudo apt-get install -y chefdk
+##########################################
+# Install the Chef Developer Kit
+#
+if [ -z "$(which chef-apply)" ]; then
+    echo "Installing Chef DK"
+    wget https://opscode-omnibus-packages.s3.amazonaws.com/debian/6/x86_64/chefdk_0.9.0-1_amd64.deb
+    sudo dpkg -i chefdk_0.9.0-1_amd64.deb
+    rm chefdk_0.9.0-1_amd64.deb
+else
+    echo "Chef DK already installed"
+fi
 
 ##########################################
 # Change shell to zsh, if not already done
