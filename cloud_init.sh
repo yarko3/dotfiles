0829842c063655a27b7002139ce02be06542b524
Benjamin Hipple <brh2113@columbia.edu>
2015-12-24 02:01:19 -0500

    more updates

--- cloud_init.sh
+++ cloud_init.sh
@@ -12,14 +12,13 @@ fi
 useradd -m -s /bin/bash "$USR"
 adduser "$USR" sudo
 
-su "USR" <<'EOF'
 cd /home/"$USR"
 mkdir .ssh
-sudo cp /root/.ssh/authorized_keys /home/"$USR"/.ssh/authorized_keys
+cp /root/.ssh/authorized_keys /home/"$USR"/.ssh/authorized_keys
 
 touch "cloud-init-in-progress.tmp"
-sudo apt-get update
-sudo apt-get install git
+apt-get update
+apt-get install git
 git clone https://github.com/brhCS/dotfiles
 cd dotfiles
 ./install
@@ -29,4 +28,5 @@ cd dotfiles_local
 ./install
 cd ..
 rm -f "cloud-init-in-progress.tmp"
-EOF
+
+chown "$USR:$USR" -R .
