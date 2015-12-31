4882f6524b723bd4f067b1d1f6c2d9eed60b76e7
Benjamin Hipple <brh2113@columbia.edu>
2015-12-30 20:16:31 -0500

    Issues with script dying

--- cloud_init.sh
+++ cloud_init.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 # To be run after booting a machine on a virtual cloud provider like DigitalOcean
 #
-# Put this as the user data:
-#!/bin/bash
-#curl https://raw.githubusercontent.com/brhCS/dotfiles/master/cloud_init.sh | bash
-#
 # Inspect its progress with:
 # less +F /var/log/cloud-init-output.log
-
 USR=bhipple
 if ! [ -z "$1" ]; then
     USR="$1"
@@ -29,15 +24,19 @@ touch "cloud-init-in-progress.tmp"
 mkdir .ssh
 cp /root/.ssh/authorized_keys /home/"$USR"/.ssh/authorized_keys
 
+echo "First $USR ownership change. You can ssh in now."
+chown "$USR:$USR" -R .
+
 echo "Setting up dotfiles"
 git clone https://github.com/brhCS/dotfiles
 cd dotfiles
 ./install
 
+echo "Finished running dotfiles installer."
 cd /home/"$USR"
-rm -f "cloud-init-in-progress.tmp"
 
 echo "Changing ownership from root to $USR"
 chown "$USR:$USR" -R .
 
 echo "Finished dotfiles cloud_init.sh"
+rm -f "cloud-init-in-progress.tmp"
