d98e8340be6a9a4b5861945f0216f2911dcf304e
Benjamin Hipple <brh2113@columbia.edu>
2015-12-30 19:05:49 -0500

    YouCompleteMe auto-build

--- bin/ycm_regular_install.sh
+++ bin/ycm_regular_install.sh
@@ -3,4 +3,13 @@
 # build-essential cmake python-dev
 
 cd ~/.vim/plugged/YouCompleteMe
-./install.py --clang-completer
+if [ -f third_party/ycmd/ycm_core.so ]; then
+    if [ "$1" == "-f" ]; then
+        ./install.py --clang-completer
+    else
+        echo "YCM already built. Use the -f flag to rebuild it."
+    fi
+else
+    ./install.py --clang-completer
+fi
+
