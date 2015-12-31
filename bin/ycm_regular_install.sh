c0846ad81e320f63589b537a516dd855934c8177
Benjamin Hipple <brh2113@columbia.edu>
2015-12-30 19:57:35 -0500

    Fault tolerance for YCM

--- bin/ycm_regular_install.sh
+++ bin/ycm_regular_install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # Requires the following packages:
 # build-essential cmake python-dev
+# When initializing in the cloud, this may happen before Chef has finished.
+cmd='./install.py --clang-complete || echo "YCM Build failed. Are its dependencies installed?"'
 
 cd ~/.vim/plugged/YouCompleteMe
 if [ -f third_party/ycmd/ycm_core.so ]; then
     if [ "$1" == "-f" ]; then
-        ./install.py --clang-completer
+        eval "$cmd"
     else
         echo "YCM already built. Use the -f flag to rebuild it."
     fi
 else
-    ./install.py --clang-completer
+    eval "$cmd"
 fi
 
