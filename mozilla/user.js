9738f277f913f91d23297f62e2b3a54d84190049
Benjamin Hipple <brh2113@columbia.edu>
2016-01-01 17:07:20 -0500

    Firefox user.js version control and auto-install

---
+++ mozilla/user.js
@@ -0,0 +1,5 @@
+user_pref("browser.sessionstore.resume_from_crash", false);
+
+// Hardware Acceleration
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("layers.offmainthreadcomposition.enabled", true);
