dc99c48848ca7184f4abcf9cf799a19c862f5271
Benjamin Hipple <brh2113@columbia.edu>
2016-01-01 20:30:22 -0500

    Enhanced volume.hs to show current vol on no args
    Also fixed a bug that prevented it from working on multiple sinks.

--- bin/volume.hs
+++ bin/volume.hs
@@ -19,11 +19,12 @@ setVol :: Float -> Int -> IO ()
 setVol t s = do
     let tar = show . round $ t / 100 * maxVol
     putStrLn $ "Setting sink " ++ show s ++ " volume to: " ++ show t ++ "% (" ++ tar ++ ")"
-    _ <- run $ "pacmd set-sink-volume 0 " ++ tar
+    _ <- run $ "pacmd set-sink-volume " ++ show s ++ " " ++ tar
     return ()
 
-volInfo :: [String] -> [String]
-volInfo = map (=~ "0-9*]%")
+sinkVol :: [String] -> [String]
+sinkVol lns = map (=~ "[0-9]*%") sinks
+    where sinks = filter (=~ "^Sink") lns
 
 sinksToIndices :: [String] -> [Int]
 sinksToIndices sx = readMany $ concat parts
@@ -32,6 +33,14 @@ sinksToIndices sx = readMany $ concat parts
 main = do
     args <- getArgs
     sinkInfo <- run "pacmd list-sinks"
-    let setter = setVol $ read . head $ args
-    let indices = sinksToIndices (lines sinkInfo)
-    mapM_ setter indices
+
+    if null args then do
+        sinkVols <- run "pacmd dump-volumes"
+        let vols = sinkVol . lines $ sinkVols
+        putStrLn "Current sink volumes:"
+        mapM_ putStrLn vols
+    else do
+        let setter = setVol $ read . head $ args
+        let indices = sinksToIndices (lines sinkInfo)
+        mapM_ setter indices
+
