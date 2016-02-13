3d742dd20a410e00d635ebf3e7c01c19b2e2dd10
Benjamin Hipple <brh2113@columbia.edu>
2016-02-13 15:29:06 -0500

    Keybinding and screen discovery updates

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,4 @@
-import System.IO (hPutStrLn)
+import System.IO (hPutStrLn, Handle)
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -6,6 +6,10 @@ import XMonad.Layout.IndependentScreens
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Run (spawnPipe)
+
+import Control.Applicative
+import Graphics.X11.Xinerama (getScreenInfo)
+import Graphics.X11.Xlib.Display (closeDisplay)
 
 myTerminal = "gnome-terminal"
 myModMask = mod4Mask
@@ -23,19 +27,28 @@ myLogHook xmproc = dynamicLogWithPP xmobarPP {
                         }
 
 restartCmd :: String
-restartCmd = "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"
+restartCmd = "if type xmonad; then xmonad --recompile && \
+              \xmonad --restart; else xmessage xmonad not in PATH; fi"
 
--- Independent Screens
-myWorkspaces :: [WorkspaceId]
-myWorkspaces = map show [1..6 :: Int]
+-- Get all Xinerama Screen #'s
+getScreens :: IO [Int]
+getScreens = openDisplay "" >>= liftA2 (<*) f closeDisplay
+    where f = fmap (zipWith const [0..]) . getScreenInfo
 
--- Where my six monitors happen to be in Xconfig
+-- Relationship between physical placement of monitor and screen number in Xinerama
 xOrder = [2, 1, 5,
           3, 0, 4]
 
+-- xmobar on every screen
+xmobarScreen :: Int -> IO Handle
+xmobarScreen = spawnPipe . ("xmobar -x " ++) . show
+
+--------------------------------------------------------------------------------
 main = do
     xmproc <- spawnPipe "xmobar"
+    screens <- getScreens
     _ <- spawn myTerminal
+    let myWorkspaces = map show screens
     xmonad $ defaultConfig
         { terminal = myTerminal
         , modMask = myModMask
@@ -46,22 +59,22 @@ main = do
         } `additionalKeys` (
         [ ((mod4Mask, xK_Return), spawn myTerminal)
         , ((controlMask .|. shiftMask, xK_l), spawn "slock")
-        , ((mod4Mask, xK_r), spawn restartCmd)
+        , ((mod4Mask, xK_q), spawn restartCmd)
         ]
 
         ++
-        -- mod-{q,w,e,a,s,d} %! Switch focus to physical/Xinerama screens
-        -- mod-shift-{q,w,e,a,s,d} %! Throw client to physical/Xinerama screen
+        -- mod-{w,e,r,s,d,f} %! Switch focus to physical/Xinerama screens
+        -- mod-shift-{w,e,r,s,d,f} %! Throw client to physical/Xinerama screen
         [ ((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-          | (key, sc) <- zip [xK_q, xK_w, xK_e, xK_a, xK_s, xK_d, xK_f] xOrder
+          | (key, sc) <- zip [xK_w, xK_e, xK_r, xK_s, xK_d, xK_f] xOrder
           , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
         ]
 
         ++
         -- mod-[1..6] %! Switch focus to workspace N (TODO: of this screen)
-        -- mod-shift-[1..6] %! Move client to workspace N (TODO: of this screen)
+        -- mod-ctrl-[1..6] %! Move client to workspace N (TODO: of this screen)
         [ ((m .|. mod4Mask, k), windows $ f i)
         | (i, k) <- zip myWorkspaces [xK_1 .. xK_9]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+        , (f, m) <- [(W.view, 0), (W.shift, controlMask)]
         ]
         )
