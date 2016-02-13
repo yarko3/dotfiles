60533de11c71f4cf8d01804e6579b5b4ce2a9dfd
Benjamin Hipple <brh2113@columbia.edu>
2016-02-13 12:11:34 -0500

    Working on my xmonad cfg

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,6 +2,8 @@ import System.IO (hPutStrLn)
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.IndependentScreens
+import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Run (spawnPipe)
 
@@ -9,9 +11,24 @@ myTerminal = "gnome-terminal"
 myModMask = mod4Mask
 
 -- Program names that should not be managed and tiled
-myManageHook = composeAll [
+composeHook = composeAll [
         className =? "Gimp" --> doFloat
     ]
+
+myManageHook = manageDocks <+> composeHook <+> manageHook defaultConfig
+myLayoutHook = avoidStruts $ layoutHook defaultConfig
+myLogHook xmproc = dynamicLogWithPP xmobarPP {
+                            ppOutput = hPutStrLn xmproc,
+                            ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+
+-- Disable greedyView
+isVisible w ws = any ((w ==) . W.tag . W.workspace) (W.visible ws)
+lazyView w ws = if isVisible w ws then ws else W.view w ws
+
+-- Independent Screens
+myWorkspaces :: [WorkspaceId]
+myWorkspaces = map show [1..6 :: Int]
 
 main = do
     xmproc <- spawnPipe "xmobar"
@@ -19,15 +36,34 @@ main = do
     xmonad $ defaultConfig {
             terminal = myTerminal,
             modMask = myModMask,
-            manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig,
-            layoutHook = avoidStruts $ layoutHook defaultConfig,
-            logHook = dynamicLogWithPP xmobarPP {
-                            ppOutput = hPutStrLn xmproc,
-                            ppTitle = xmobarColor "green" "" . shorten 50
-                        }
+            XMonad.workspaces = myWorkspaces,
+            manageHook = myManageHook,
+            layoutHook = myLayoutHook,
+            logHook = myLogHook xmproc
         } `additionalKeys`
-        [
-            ((mod4Mask, xK_Return), spawn myTerminal),
-            ((controlMask .|. shiftMask, xK_l), spawn "slock")
+        [((mod4Mask, xK_Return), spawn myTerminal)
+        ,((controlMask .|. shiftMask, xK_l), spawn "slock")
         ]
+        {--
+        ++
+        -- mod-[1..9] %! Switch to workspace N
+        -- mod-shift-[1..9] %! Move client to workspace N
+        [((m .|. mod4Mask, k), windows $ f i)
+            | (i, k) <- zip myWorkspaces [xK_1 .. xK_9]
+            , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        ++
+        -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+        -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+        [((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+            | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+            , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+        --}
+         -- ++
+         --
+         -- mod-[1..9], Switch to workspace N
+         -- mod-shift-[1..9], Move client to workspace N
+         --
+--        [((m .|. mod4Mask, k), windows $ f i)
+--        , (i, k) <- zip (myWorkspaces) [xK_1 .. xK_9]
+--        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
