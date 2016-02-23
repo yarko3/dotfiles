0f8debeb9560241ec37db70a4b41c2f828cf50d2
Benjamin Hipple <brh2113@columbia.edu>
2016-02-23 01:17:39 -0500

    added haskell stack install

---
+++ shell/stack.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if ! [ "$(which stack)" ]; then
+    echo "Installing Stack"
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 575159689BEFB442
+    if [ "$(lsb_release -a | grep -q '15.10')" ]; then
+        echo 'deb http://download.fpcomplete.com/ubuntu wily main'|sudo tee /etc/apt/sources.list.d/fpco.list
+    else
+        echo "Not sure what version to add."
+        echo "See: https://github.com/commercialhaskell/stack/blob/master/doc/install_and_upgrade.md#ubuntu"
+        exit 1
+    fi
+
+    sudo apt-get update
+    sudo apt-get install stack
+fi
+
+stack setup
+stack install hdevtools
