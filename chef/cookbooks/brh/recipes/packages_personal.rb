cd4f5abb7940c7b6759ceec7da0a68b7889c50b4
Benjamin Hipple <brh2113@columbia.edu>
2016-02-23 00:59:28 -0500

    Get Stack from PPA, not from default Ubuntu distro

--- chef/cookbooks/brh/recipes/packages_personal.rb
+++ chef/cookbooks/brh/recipes/packages_personal.rb
@@ -46,7 +46,6 @@ package 'python-sklearn'
 
 # Haskell
 #package 'hoogle' # Installs Apache2, which causes issues on DigitalOcean?
-package 'haskell-stack'
 package 'libghc-regex-posix-dev'
 package 'pandoc'
 package 'texlive-latex-recommended'
