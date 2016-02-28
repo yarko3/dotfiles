1bc3bc8bca0482cf52baf13eae72ddee89b8102a
Benjamin Hipple <brh2113@columbia.edu>
2016-02-27 20:30:34 -0500

    Simple apt alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
+alias ai='sudo apt-get install'
 alias battery='upower -i /org/freedesktop/UPower/devices/battery_BAT0'
 alias be='bundle exec'
 alias beg='bundle exec guard'
@@ -17,6 +18,7 @@ alias rfwifi='nmcli r wifi off && nmcli r wifi on'
 alias sbcl='rlwrap sbcl'
 alias scheme='rlwrap scheme'
 alias sqlite3='rlwrap sqlite3'
+alias uu='sudo apt-get update && sudo apt-get upgrade'
 alias wfc='weather -f NYC'
 
 [ -f ~/.zsh_local/aliases_local.zsh ] && source ~/.zsh_local/aliases_local.zsh
