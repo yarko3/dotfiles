63e318e6c13c7010f3b2cb634baaebc975dcd765
Benjamin Hipple <brh2113@columbia.edu>
2016-03-05 15:24:32 -0500

    Updates for MacOS

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -28,6 +28,8 @@ PATH=$PATH:/usr/sbin
 
 export PYTHONPATH=~/bin
 
+export LIBRARY_PATH="/opt/X11/lib:$LIBRARY_PATH"
+
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
 
