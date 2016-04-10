992424b06726042191942089a76a8b0a86c0b631
Benjamin Hipple <bhipple@bloomberg.net>
2016-04-10 00:52:00 -0400

    Moving Mozilla configuration into local dotfiles

--- mozilla/user.js
+++
@@ -1,5 +0,0 @@
-user_pref("browser.sessionstore.resume_from_crash", false);
-
-// Hardware Acceleration
-user_pref("layers.acceleration.force-enabled", true);
-user_pref("layers.offmainthreadcomposition.enabled", true);
