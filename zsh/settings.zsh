f2bae6a2da03e67831e54e74e33b24f8d1ce0701
yarko3 <yarko3@hotmail.com>
2016-10-16 11:11:48 -0400

    removing character from prompt

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -87,7 +87,7 @@ bindkey -v
 ## ============================================================================
 ##                                  Prompt
 ## ============================================================================
-PROMPT='%{$fg[yellow]%}λ %m %{$fg[green]%}%c%{$fg[yellow]%} →  %{$reset_color%}'
+PROMPT='%{$fg[yellow]%}%m %{$fg[green]%}%c%{$fg[yellow]%} →  %{$reset_color%}'
 
 ZSH_THEME_GIT_PROMPT_PREFIX=""
 ZSH_THEME_GIT_PROMPT_SUFFIX=""
