233f2d6f13730aaf0cb469639684ca20db0e17b4
Yaroslav Senyuta <yarko3@hotmail.com>
2017-02-22 20:49:38 -0500

    made fshow formatted like g lg

--- bash/functions.sh
+++ bash/functions.sh
@@ -4,8 +4,8 @@
 
 # fshow - git commit browser
 fshow() {
-  git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  git log --graph --oneline --branches --decorate --color=always \
+      --format=format:'%C(bold blue)%h%C(reset) - %C(bold blue)(%ar) %C(bold yellow)%d%C(reset) %C(green)%s%C(reset) %C(dim green)- %an%C(reset)' "$@" |
   fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
       --bind "ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
