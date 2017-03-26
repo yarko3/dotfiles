134ada6a35cc9ef6b2ffc815e1a19a38944147f7
Yaroslav Senyuta <yarko3@hotmail.com>
2017-03-26 18:00:04 -0400

    adding an alias for ls to have colors

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
+alias :q='exit'
 alias cdr='cd $(git rev-parse --show-toplevel)'
 alias fn='find . -name'
 alias g='git'
@@ -11,6 +12,7 @@ alias hd='~/bin/hex_decimal.sh'
 alias less='less -N'
 alias ll='ls -al'
 alias lower="tr '[:upper:]' '[:lower:]'"
+alias ls='ls --color=auto'
 alias m='make -j'
 alias mkcd='. ~/bin/make_dir_and_cd.sh'
 alias mt='make -j test'
@@ -22,7 +24,6 @@ alias topcpu='/bin/ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10'
 alias upper="tr '[:lower:]' '[:upper:]'"
 alias uu='sudo apt-get update && sudo apt-get upgrade'
 alias wfc='weather -f NYC'
-alias :q='exit'
 
 [ -f ~/.zsh_local/aliases_local.zsh ] && source ~/.zsh_local/aliases_local.zsh
 
