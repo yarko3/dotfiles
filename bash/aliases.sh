857be6621af88154fb0365ad9084408973fc7f8c
Yaroslav Senyuta <ysenyuta@bloomberg.net>
2017-04-25 10:51:33 -0400

    remove sourcing of zsh aliases in bash

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,8 +1,5 @@
 # =============================================================================
 #                                  Aliases
 # =============================================================================
-# Source my generic aliases
-[ -f ~/.zsh/aliases.zsh ] && source ~/.zsh/aliases.zsh
-
 # Source local aliases
 [ -f ~/.bashrc_local/bashrc_local_aliases.sh ] && source ~/.bashrc_local/bashrc_local_aliases.sh
