08365c0909e5c34b9c76e614b8bbbc9a9c05fa0a
Yaroslav Senyuta <ysenyuta@bloomberg.net>
2017-06-21 08:44:36 -0400

    make tmux update script kill session afterwards

--- scripts/install_tmux_plugins.sh
+++ scripts/install_tmux_plugins.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+UPDATE_SESSION_NAME=updatePlugins
+
 # start a server but don't attach to it
 tmux start-server
 # create a new session but don't attach to it either
-tmux new-session -d
+tmux new-session -d -s $UPDATE_SESSION_NAME
 # install the plugins
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 # update plugins
 ~/.tmux/plugins/tpm/bin/update_plugins all
-# killing the server is not required
-# tmux kill-server
+# kill session
+tmux kill-session -t $UPDATE_SESSION_NAME
