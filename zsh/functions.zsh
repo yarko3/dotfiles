84bb76ea82e3fa2d58a1a4dd1a8c411cbf5dc142
Yaroslav Senyuta <ysenyuta@bloomberg.net>
2017-08-11 13:10:52 -0400

    function cleanup

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,17 +1,3 @@
-function vimify() {
-    (vim - -esbnN -c "$*" -c 'w!/dev/fd/3|q!' >/dev/null) 3>&1
-}
-
-function resize() {
-    file="$1"
-    dimensions="800x800"
-    if [ $# -eq 2 ]; then
-        dimensions="$2"
-    fi
-
-    convert "$file" -resize "$dimensions" "$1"
-}
-
 # git commit browser
 function glg() {
   git log --graph --oneline --branches --decorate --color=always \
