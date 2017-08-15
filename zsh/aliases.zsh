0e4bdfd696bce6269d5b811955f1058209c43a40
Yaroslav Senyuta <ysenyuta@bloomberg.net>
2017-08-15 16:18:33 -0400

    update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,6 @@ else
     alias uu='sudo apt-get update && sudo apt-get upgrade'
 fi
 
-alias ai='sudo apt-get install'
 alias cdr='cd $(git rev-parse --show-toplevel)'
 alias fn='find . -name'
 alias g='git'
@@ -18,16 +17,12 @@ alias gmc='gmake -j clean'
 alias gmr='gmake clean -j && gmake -j'
 alias gmt='gmake test -j'
 alias grep='grep --color=auto'
-alias hd='~/bin/hex_decimal.sh'
 alias less='less -N'
-alias ll='ls -al'
+alias ll='ls -lrta'
 alias lower="tr '[:upper:]' '[:lower:]'"
 alias m='make -j'
-alias mkcd='. ~/bin/make_dir_and_cd.sh'
 alias mt='make -j test'
-alias myip='curl cmyip.com 2>/dev/null | grep -o "My IP Address is [0-9.]*"'
 alias p='ps aux | grep '
-alias rfwifi='nmcli r wifi off && nmcli r wifi on'
 alias tmux='tmux -2u'
 alias topcpu='/bin/ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10'
 alias upper="tr '[:lower:]' '[:upper:]'"
