cfc63472c380c80d02d9f66194ed3ace6900136f
Yaroslav Senyuta <yarko3@hotmail.com>
2017-10-17 11:50:03 -0400

    make update env script more robust

--- scripts/update_env.sh
+++ scripts/update_env.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
 update_nix() {
-    if ! which nix-env 2>&1 ; then
-        [[ -f ~/.nix-profile/etc/profile.d/nix.sh ]] && . ~/.nix-profile/etc/profile.d/nix.sh
+    update_nix_cmd() {
+        nix-env -j 4 -u
+    }
+
+    echo "Updating nix..."
+
+    if ! which nix-env > /dev/null 2>&1 ; then
+        if [[ -f ~/.nix-profile/etc/profile.d/nix.sh ]]; then
+            . ~/.nix-profile/etc/profile.d/nix.sh && update_nix_cmd
+        else:
+            echo "WARNING: nix not detected."
+        fi
+    else
+        update_nix_cmd
     fi
-    nix-env -j 4 -u
+
+    echo "Finished updating nix"
 }
 
 update_native() {
+    echo "Updating local environment..."
+
     if [ "$(uname)" = "Darwin" ]; then
         brew update && brew upgrade
     else
         sudo apt-get update && sudo apt-get upgrade
     fi
-}
 
-echo "Updating local environment..."
+    echo "Finished updating local environment"
+}
 
 update_nix
 update_native
-
-echo "Finished updating local environment"
