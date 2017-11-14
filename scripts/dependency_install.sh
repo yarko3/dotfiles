db469bdbfe553d25b88e65d360c2dcd13fd38f7e
Yaroslav Senyuta <yarko3@hotmail.com>
2017-11-13 23:50:36 -0500

    dependency script should try to install fzf; use new tabstop

--- scripts/dependency_install.sh
+++ scripts/dependency_install.sh
@@ -3,50 +3,46 @@
 DOTFILES_DIR="$HOME/.dotfiles"
 
 change_to_zsh() {
-    if [ "$(echo "$SHELL" | grep -c "zsh")" -eq "0" ]; then
-        echo "Setting shell to zsh"
-        chsh -s "$(which zsh)"
-    else
-        echo "zsh is already the default shell"
-    fi
+  if [ "$(echo "$SHELL" | grep -c "zsh")" -eq "0" ]; then
+    echo "Setting shell to zsh"
+    chsh -s "$(which zsh)"
+  else
+    echo "zsh is already the default shell"
+  fi
 }
 
 create_ssh() {
-    mkdir -p "$HOME"/.ssh
-    chmod 0700 "$HOME"/.ssh
+  mkdir -p "$HOME"/.ssh
+  chmod 0700 "$HOME"/.ssh
 }
 
 fzf_install() {
-    if ! [ -f ~/.fzf.zsh ]; then
-        echo "Installing fzf"
-        "$DOTFILES_DIR"/fzf/install --key-bindings --completion --no-update-rc
-    else
-        echo "fzf already installed"
-    fi
+  echo "Installing fzf"
+  "$DOTFILES_DIR"/fzf/install --key-bindings --completion --no-update-rc
 }
 
 fonts_install() {
-    if ! [ -d ~/.local/share/fonts ]; then
-        "$DOTFILES_DIR"/fonts/install.sh
-    else
-        echo "fonts already installed"
-    fi
+  if ! [ -d ~/.local/share/fonts ]; then
+    "$DOTFILES_DIR"/fonts/install.sh
+  else
+    echo "fonts already installed"
+  fi
 }
 
 local_install() {
-    echo "Installing local packages..."
-    if [ "$(uname)" = "Linux" ]; then
-        "$DOTFILES_DIR"/scripts/apt_setup.sh
-    else
-        echo "No local packages to install..."
-    fi
-    echo "Finished installing local packages"
+  echo "Installing local packages..."
+  if [ "$(uname)" = "Linux" ]; then
+    "$DOTFILES_DIR"/scripts/apt_setup.sh
+  else
+    echo "No local packages to install..."
+  fi
+  echo "Finished installing local packages"
 }
 
 install() {
-    fzf_install
-    fonts_install
-    local_install
+  fzf_install
+  fonts_install
+  local_install
 }
 
 create_ssh
