cc1482b78eb418bdc040fc7a3e0b0c506f78b301
Yaroslav Senyuta <ysenyuta@google.com>
2017-12-10 12:21:50 -0500

    misc: fix indents

--- bin/epoch.sh
+++ bin/epoch.sh
@@ -1,40 +1,40 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 usage()
 {
-    echo "$0 usage: <epoch_seconds|epoch_millis>"
-    exit 1
+  echo "$0 usage: <epoch_seconds|epoch_millis>"
+  exit 1
 }
 
 convert_to_epoch_secs()
 {
-    epoch=$1
-    num_chars_in_epoch_secs=10
-    # assuming nobody is gonna be using this script past 2286; plz prove me wrong
-    if [ ${#epoch} -gt $num_chars_in_epoch_secs ]; then
-        echo $((epoch / 1000))
-    else
-        echo "$epoch"
-    fi
+  epoch=$1
+  num_chars_in_epoch_secs=10
+  # assuming nobody is gonna be using this script past 2286; plz prove me wrong
+  if [ ${#epoch} -gt $num_chars_in_epoch_secs ]; then
+    echo $((epoch / 1000))
+  else
+    echo "$epoch"
+  fi
 }
 
 print_epoch_secs()
 {
-    if date --version 2>/dev/null | grep "GNU coreutils" > /dev/null 2>&1; then
-        date -d @"$1"
-    else
-        date -r "$1"
-    fi
+  if date --version 2>/dev/null | grep "GNU coreutils" > /dev/null 2>&1; then
+    date -d @"$1"
+  else
+    date -r "$1"
+  fi
 }
 
 main()
 {
-    epoch_seconds=$(convert_to_epoch_secs "$1")
-    print_epoch_secs "$epoch_seconds"
+  epoch_seconds=$(convert_to_epoch_secs "$1")
+  print_epoch_secs "$epoch_seconds"
 }
 
 if [ $# -eq 1 ]; then
-    main "$1"
+  main "$1"
 else
-    usage
+  usage
 fi
