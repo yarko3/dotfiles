441c50c9e3cc2e6d18a8cf50730310df81633c36
Yaroslav Senyuta <ysenyuta@google.com>
2018-03-11 21:37:42 -0400

    zsh: clean up unused functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -10,25 +10,11 @@ function glg() {
 FZF-EOF"
 }
 
-function noproxy() {
-    env -u http_proxy -u https_proxy -u HTTP_PROXY -u HTTPS_PROXY "$@"
-}
-
 pathDeduplicate() {
     export PATH="$(echo "$PATH" |
         awk 'BEGIN{RS=":";}
             {sub(sprintf("%c$",10),"");if(A[$0]){}else{A[$0]=1;printf(((NR==1)?"":":")$0)}}' \
         )";
-}
-
-function logview {
-    if (( $# == 3 )); then
-        ls -rt $1/$2.log* | tail -$3 | head -1
-    elif (( $# == 2 )); then
-        less -n -S $(ls -rt $1/$2.log* | tail -1)
-    else
-        less -n -S $(ls -rt ${LOCAL_LOG_DIR}/$1.log* | tail -1)
-    fi
 }
 
 unalias z 2> /dev/null
