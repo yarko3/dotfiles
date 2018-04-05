8bac0da06335de2233c4c2ec180115a13120cffd
Yaroslav Senyuta <ysenyuta@google.com>
2018-04-05 15:31:36 -0400

    misc: install build module in dependency install

--- scripts/dependency_install.sh
+++ scripts/dependency_install.sh
@@ -45,6 +45,7 @@ local_install() {
 rainbarf_install() {
   echo "Installing rainbarf..."
   if ! [ -x ~/lib/perl5/bin/rainbarf ]; then
+    cpan Module::Build
     cd "$DOTFILES_DIR"/rainbarf
     perl Build.PL --install_base ~/lib/perl5
     ./Build test
