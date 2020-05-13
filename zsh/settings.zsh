1b406dde1e15e58daac9ee429b3444a53834b0c6
Yaroslav Senyuta <ysenyuta@google.com>
2020-05-13 09:54:45 -0400

    zsh: update fzf completion locations for use with apt

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -76,4 +76,5 @@ zle -N zle-keymap-select
 #  ============================================================================
 #                                   FZF
 #  ============================================================================
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
+[ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
