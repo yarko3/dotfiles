b4c4895e4d3baddd7a78fb9636f55bf6d8d46c63
Yaroslav Senyuta <ysenyuta@google.com>
2021-11-22 15:20:17 -0500

    zsh: add homebrew if installed locally

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -59,6 +59,7 @@ PATH=$PATH:~/neovim/bin
 PATH=$PATH:/bin
 PATH=$PATH:/sbin
 PATH=$PATH:/usr/local/bin
+PATH=$HOME/homebrew/bin:$PATH
 PATH=$PATH:/usr/bin
 PATH=$PATH:/usr/local/sbin
 PATH=$PATH:/usr/sbin
